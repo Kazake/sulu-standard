43421427542ef5794a115941875ffadf02a9a553
Wachter Johannes <johannes.wachter@massiveart.at>
2015-01-12 09:09:41 +0100

    removed unused config

--- app/AbstractKernel.php
+++ app/AbstractKernel.php
@@ -52,6 +52,7 @@ abstract class AbstractKernel extends SuluKernel
             new Sulu\Bundle\WebsiteBundle\SuluWebsiteBundle(),
             new Sulu\Bundle\LocationBundle\SuluLocationBundle(),
             new Sulu\Bundle\SearchBundle\SuluSearchBundle(),
+            new \Sulu\Bundle\WebsocketBundle\SuluWebsocketBundle(),
 
             // website
             new Client\Bundle\WebsiteBundle\ClientWebsiteBundle(),
